2730ddff58269782b9474474af98f5546bfd68c1
Jaqueline <133174675+jueslyyy@users.noreply.github.com>
2024-06-25 09:44:47 -0300

    Endpoint Read By ID '[GET] /personagem/:id' criado

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ const list =['Java', 'Kotlin','Android']
 
 // Endpoint Read All [GET] /personagem
 
-app.get('/personagem', function(req, res) {
+app.get('/personagem', function (req, res) {
     res.send(list)
+})
+
+//Endpoint Read By ID [GET] /personagem/:id
+app.get('/personagem/:id', function (req, res) {
+  //Acessamos o parâmetro de rota ID
+    const id = req.params.id
+
+  // Acessa o item na lista usando o ID -1  
+    const item = list[id -1]
+
+  //Enviamos o item como resposta
+    res.send(item)
+
 })
 
 app.listen(3000)
