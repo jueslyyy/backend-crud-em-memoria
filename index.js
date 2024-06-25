9d3ef907f1e3da0cfe5b2ec6961fd2cdae25222a
Jaqueline <133174675+jueslyyy@users.noreply.github.com>
2024-06-25 09:10:32 -0300

    Endpoint 'Read All [GET]/PERSONAGEM' implementado

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World')
+})
+
+const list =['Java', 'Kotlin','Android']
+
+// Endpoint Read All [GET] /personagem
+
+app.get('/personagem', function(req, res) {
+    res.send(list)
 })
 
 app.listen(3000)
